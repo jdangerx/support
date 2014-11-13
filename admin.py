c2f444a5ae5539f7da07aa3cbd381f6cdb33a73b
Carrie McLaughlin <camclaug@gmail.com>
2014-11-13 11:58:41 -0800

    editable ordering on list pages

--- admin.py
+++ admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
 from support.models import Grade, Unit, Lesson, SupplementalMaterial, Question, Answer, Topic, TopicGrade, Vote, LessonTopic, Forum
 
-admin.site.register(Grade)
-admin.site.register(Unit)
-admin.site.register(SupplementalMaterial)
+
 admin.site.register(Question)
 admin.site.register(Answer)
-admin.site.register(Topic)
+
 admin.site.register(Vote)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'order')
+    list_editable = ['order']
+admin.site.register(Grade, OrderAdmin)
+admin.site.register(Unit, OrderAdmin)
+admin.site.register(SupplementalMaterial, OrderAdmin)
+admin.site.register(Topic, OrderAdmin)
 
 #@admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
     exclude = ('forum',)
-    list_display = ('__str__', 'forum')
+    list_display = ('__str__', 'forum','order')
+    list_editable = ['order']
 
     def save_model(self, request, obj, form, change):
         if change == False:
