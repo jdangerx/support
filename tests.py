6679ad8fc2f75bfe0c9455997f3863290de6e79c
Carrie McLaughlin <camclaug@gmail.com>
2015-02-05 15:53:26 -0800

    disable support tests

--- tests.py
+++ tests.py
@@ -3,8 +3,8 @@ from django.core.urlresolvers import reverse
 # Create your tests here.
 
 
-class LessonViewTests(TestCase):
-	def test_questions_show_in_order(self):
-		lesson = 
-		response = self.client.get(reverse('support:lesson'))
+#class LessonViewTests(TestCase):
+#	def test_questions_show_in_order(self):
+#		lesson = 
+#		response = self.client.get(reverse('support:lesson'))
 
